48e0065d48ce0eb638a2aae58fe786e373e14794
Mohamed Saleh <mohamed20163858@gmail.com>
2022-11-23 00:43:06 +0200

    Fix rubocop errors

--- person.rb
+++ person.rb
@@ -2,14 +2,14 @@ class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = nil
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 
@@ -19,6 +19,6 @@ class Person
     false
   end
 
-  private :is_of_age?
+  private :of_age?
   public :can_use_services?
 end
